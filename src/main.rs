900cab83eef2f7494171acecd428c374ddd8d308
Lethios <lethios@protonmail.com>
2026-08-24 17:35:09 +0530

    refactor: move quit, pause and draw into their own functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use crossterm::event::KeyCode;
 use crossterm::{cursor, event, execute, style, terminal};
 use std::io;
 
@@ -30,6 +31,84 @@ fn render(game: &Game) -> io::Result<()> {
     execute!(io::stdout(), cursor::MoveTo(0, 0), style::Print(output))
 }
 
+fn quit() -> io::Result<()> {
+    execute!(
+        io::stdout(),
+        terminal::LeaveAlternateScreen,
+        cursor::Show,
+        cursor::SetCursorStyle::DefaultUserShape
+    )?;
+    terminal::disable_raw_mode()?;
+
+    std::process::exit(0)
+}
+
+fn pause(rows: u16, cols: u16) -> io::Result<()> {
+    loop {
+        if let event::Event::Key(k) = event::read()?
+            && k.kind == event::KeyEventKind::Press
+        {
+            match k.code {
+                event::KeyCode::Char('p') => {
+                    break;
+                }
+                event::KeyCode::Char(' ') => draw(rows, cols)?,
+                event::KeyCode::Char('q') | event::KeyCode::Esc => quit()?,
+                _ => {}
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn draw(rows: u16, cols: u16) -> io::Result<()> {
+    execute!(
+        io::stdout(),
+        cursor::MoveTo(cols / 2, rows / 2),
+        cursor::SetCursorStyle::BlinkingUnderScore,
+        cursor::Show
+    )?;
+
+    let mut draw_mode = false;
+
+    loop {
+        if let event::Event::Key(k) = event::read()? {
+            if matches!(
+                k.kind,
+                event::KeyEventKind::Press | event::KeyEventKind::Repeat
+            ) {
+                match k.code {
+                    event::KeyCode::Char('p') => {
+                        // temp
+                    }
+                    event::KeyCode::Char(' ') => {
+                        draw_mode = !draw_mode;
+
+                        if draw_mode {
+                            // temp
+                        }
+                    }
+                    event::KeyCode::Char('w') | event::KeyCode::Up => {
+                        execute!(io::stdout(), cursor::MoveUp(1))?
+                    }
+                    event::KeyCode::Char('a') | event::KeyCode::Left => {
+                        execute!(io::stdout(), cursor::MoveLeft(1))?
+                    }
+                    event::KeyCode::Char('s') | event::KeyCode::Down => {
+                        execute!(io::stdout(), cursor::MoveDown(1))?
+                    }
+                    event::KeyCode::Char('d') | event::KeyCode::Right => {
+                        execute!(io::stdout(), cursor::MoveRight(1))?
+                    }
+                    event::KeyCode::Char('q') | event::KeyCode::Esc => quit()?,
+                    _ => {}
+                }
+            }
+        }
+    }
+}
+
 fn main() -> io::Result<()> {
     // Parse arguments from command line
     let args = parse_args().unwrap_or_else(|e| {
@@ -42,51 +121,30 @@ fn main() -> io::Result<()> {
     let (cols, rows) = terminal::size().expect("Error in fetching terminal size.");
     let grid = Grid::new_random(rows, cols, args.spawn, args.seed);
     let mut game = Game::new(grid, args.rulestring);
-    let mut is_paused = false;
 
     terminal::enable_raw_mode()?;
     execute!(io::stdout(), terminal::EnterAlternateScreen, cursor::Hide)?;
 
-    'outer: loop {
+    loop {
         let frame_start = std::time::Instant::now();
 
         render(&game)?;
         game.tick();
 
-        // Check if p is pressed
+        // Check if a key is pressed
         if event::poll(std::time::Duration::ZERO)?
             && let event::Event::Key(k) = event::read()?
             && k.kind == event::KeyEventKind::Press
-            && matches!(k.code, event::KeyCode::Char('p'))
         {
-            is_paused = true;
-        }
+            match k.code {
+                // Pause if p is pressed
+                event::KeyCode::Char('p') => pause(rows, cols)?,
 
-        // Pause game until p is pressed again
-        if is_paused {
-            loop {
-                if let event::Event::Key(k) = event::read()?
-                    && k.kind == event::KeyEventKind::Press
-                {
-                    if matches!(k.code, event::KeyCode::Char('p')) {
-                        is_paused = false;
-                        break;
-                    }
+                // Exit loop if q is pressed
+                event::KeyCode::Char('q') | event::KeyCode::Esc => break,
 
-                    if matches!(k.code, event::KeyCode::Char('q') | event::KeyCode::Esc) {
-                        break 'outer;
-                    }
-                }
+                _ => {}
             }
-        }
-
-        // Exit if q or Esc pressed
-        if event::poll(std::time::Duration::ZERO)?
-            && let event::Event::Key(k) = event::read()?
-            && k.kind == event::KeyEventKind::Press
-            && matches!(k.code, event::KeyCode::Char('q') | event::KeyCode::Esc)
-        {
-            break;
         }
 
         // Cap frame rate by sleeping for the remaining time in the frame
